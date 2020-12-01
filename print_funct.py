9d51dc8e4d2784587d7b4a4e9265dfd7fc820d1c
Lisa <72004449+Linellian@users.noreply.github.com>
2020-12-01 17:05:02 +0300

    Update print_funct.py

--- print_funct.py
+++ print_funct.py
@@ -1,22 +1,25 @@
+import base_funct
+
+
 def f_print(st_a, st_b):
-    if f_len(st_a) > f_len(st_b):
-        for i in range(f_len(st_b)):
+    if base_funct.f_len(st_a) > base_funct.f_len(st_b):
+        for i in range(base_funct.f_len(st_b)):
             print(st_a[i], st_b[i])
-        for i in range(-(f_len(st_a) - f_len(st_b)), 0):
+        for i in range(-(base_funct.f_len(st_a) - base_funct.f_len(st_b)), 0):
             print(st_a[i])
         print("")
         print("_  _")
         print("a  b")
-    elif f_len(st_a) < f_len(st_b):
-        for i in range(f_len(st_a)):
+    elif base_funct.f_len(st_a) < base_funct.f_len(st_b):
+        for i in range(base_funct.f_len(st_a)):
            print(st_a[i], st_b[i])
-        for i in range(-(f_len(st_b) - f_len(st_a)), 0):
+        for i in range(-(base_funct.f_len(st_b) - base_funct.f_len(st_a)), 0):
             print(" ", st_b[i])
         print("")
         print("_  _")
         print("a  b")
-    elif f_len(st_a) == f_len(st_b):
-        for i in range(f_len(st_b)):
+    elif base_funct.f_len(st_a) == base_funct.f_len(st_b):
+        for i in range(base_funct.f_len(st_b)):
             print(st_a[i], st_b[i])
         print("")
         print("_  _")
