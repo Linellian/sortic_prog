e17b55109c835633afeb5b48b4d073e497a1faf4
Lisa <72004449+Linellian@users.noreply.github.com>
2020-12-16 21:28:54 +0300

    Update sortic.py

--- sortic.py
+++ sortic.py
@@ -4,27 +4,38 @@ import r_funct
 import rr_funct
 
 
-def sortic(a, b):
+def f_sortic(a, b):
+    a, b = sp_funct.pb(a, b)
+    a, b = sp_funct.pb(a, b)
     if a != []:
         while base_funct.f_len(a) != 0:
             if a[0] > base_funct.f_min(b) and a[0] < base_funct.f_max(b):
                 if a[0] > b[0]:
                     while a[0] > b[0]:
                         b = r_funct.rb(b)
+                        print("rb")
                     a, b = sp_funct.pb(a, b)
+                    print("pb")
                 elif a[0] < b[0]:
                     while a[0] < b[0]:
                         b = rr_funct.rrb(b)
+                        print("rrb")
                     a, b = sp_funct.pb(a, b)
+                    print("pb")
                     b = sp_funct.sb(b)
+                    print("sb")
             elif a[0] < base_funct.f_min(b) or a[0] > base_funct.f_max(b):
                 while b[0] != base_funct.f_max(b):
                     b = rr_funct.rrb(b)
+                    print("rrb")
                 a, b = sp_funct.pb(a, b)
+                print("pb")
                 b = sp_funct.sb(b)
+                print("sb")
 
         while b[0] != base_funct.f_min(b):
             b = r_funct.rb(b)
+            print("rb")
         a, b = b, []
     return a
 
@@ -37,7 +48,5 @@ while el_a != "!":
 
 # a = [2, 1, 3]
 b = []
-a, b = sp_funct.pb(a, b)
-a, b = sp_funct.pb(a, b)
 
-sortic(a, b)
+a = f_sortic(a, b)
