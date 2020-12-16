7982d5680cf141773c77532ea0e32a597bde3e4a
Lisa <72004449+Linellian@users.noreply.github.com>
2020-12-16 22:28:23 +0300

    Update sortic.py

--- sortic.py
+++ sortic.py
@@ -1,43 +1,4 @@
-import base_funct
-import sp_funct
-import r_funct 
-import rr_funct
-
-
-def f_sortic(a, b):
-    a, b = sp_funct.pb(a, b)
-    a, b = sp_funct.pb(a, b)
-    if a != []:
-        while base_funct.f_len(a) != 0:
-            if a[0] > base_funct.f_min(b) and a[0] < base_funct.f_max(b):
-                if a[0] > b[0]:
-                    while a[0] > b[0]:
-                        b = r_funct.rb(b)
-                        print("rb")
-                    a, b = sp_funct.pb(a, b)
-                    print("pb")
-                elif a[0] < b[0]:
-                    while a[0] < b[0]:
-                        b = rr_funct.rrb(b)
-                        print("rrb")
-                    a, b = sp_funct.pb(a, b)
-                    print("pb")
-                    b = sp_funct.sb(b)
-                    print("sb")
-            elif a[0] < base_funct.f_min(b) or a[0] > base_funct.f_max(b):
-                while b[0] != base_funct.f_max(b):
-                    b = rr_funct.rrb(b)
-                    print("rrb")
-                a, b = sp_funct.pb(a, b)
-                print("pb")
-                b = sp_funct.sb(b)
-                print("sb")
-
-        while b[0] != base_funct.f_min(b):
-            b = r_funct.rb(b)
-            print("rb")
-        a, b = b, []
-    return a
+import sortic_funct
 
 
 el_a = input()
@@ -47,6 +8,5 @@ while el_a != "!":
     el_a = input()
 
 # a = [2, 1, 3]
-b = []
 
-a = f_sortic(a, b)
+a = sortic_funct.f_sort(a)
