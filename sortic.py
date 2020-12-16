98bbcdf131a4de225e68c0084b6439cb08d0ccaa
Lisa <72004449+Linellian@users.noreply.github.com>
2020-12-16 20:30:20 +0300

    Update sortic.py

--- sortic.py
+++ sortic.py
@@ -4,13 +4,40 @@ import r_funct
 import rr_funct
 
 
-a = input()
-st_a = []
-while a != "!":
-    st_a.append(int(a))
-    a = input()
+def sortic(a, b):
+    if a != []:
+        while base_funct.f_len(a) != 0:
+            if a[0] > base_funct.f_min(b) and a[0] < base_funct.f_max(b):
+                if a[0] > b[0]:
+                    while a[0] > b[0]:
+                        b = r_funct.rb(b)
+                    a, b = sp_funct.pb(a, b)
+                elif a[0] < b[0]:
+                    while a[0] < b[0]:
+                        b = rr_funct.rrb(b)
+                    a, b = sp_funct.pb(a, b)
+                    b = sp_funct.sb(b)
+            elif a[0] < base_funct.f_min(b) or a[0] > base_funct.f_max(b):
+                while b[0] != base_funct.f_max(b):
+                    b = rr_funct.rrb(b)
+                a, b = sp_funct.pb(a, b)
+                b = sp_funct.sb(b)
+
+        while b[0] != base_funct.f_min(b):
+            b = r_funct.rb(b)
+        a, b = b, []
+    return a
 
 
-# Комментарии с ожидаемыми выводами актуальны для значения
-st_b = [1, 2, 3, 4, 5]
-st_a, st_b = base_funct.f_sortic(st_a, st_b)
+el_a = input()
+a = []
+while el_a != "!":
+    a.append(int(el_a))
+    el_a = input()
+
+# a = [2, 1, 3]
+b = []
+a, b = sp_funct.pb(a, b)
+a, b = sp_funct.pb(a, b)
+
+sortic(a, b)
