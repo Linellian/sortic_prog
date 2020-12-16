e44891943b75b1b3d24e764d3fcd429fffccf310
Lisa <72004449+Linellian@users.noreply.github.com>
2020-12-16 21:28:18 +0300

    Update sp_funct.py

--- sp_funct.py
+++ sp_funct.py
@@ -4,28 +4,19 @@ import base_funct
 def sa(st_a):
     if base_funct.f_len(st_a) >= 2:
         st_a[0], st_a[1] = st_a[1], st_a[0]
-        print("sa")
-        return st_a
-    else:
-        return st_a
+    return st_a
 
 def sb(st_b):
     if base_funct.f_len(st_b) >= 2:
         st_b[0], st_b[1] = st_b[1], st_b[0]
-        print("sb")
-        return st_b
-    else:
-        return st_b
+    return st_b
 
 def ss(st_a, st_b):
     if base_funct.f_len(st_a) >= 2 and base_funct.f_len(st_b) >= 2:
         st_a[0], st_a[1] = st_a[1], st_a[0]
         st_b[0], st_b[1] = st_b[1], st_b[0]
-        print("ss")
-        return st_a, st_b
-    else:
-        return st_a, st_b
-
+    return st_a, st_b
+    
 
 def pa(st_a, st_b):
     if base_funct.f_len(st_b) != 0:
@@ -34,10 +25,7 @@ def pa(st_a, st_b):
             c.append(st_a[i])
         st_a = c
         st_b = base_funct.f_del_first(st_b)
-        print("pa")
-        return st_a, st_b
-    else:
-        return st_a, st_b
+    return st_a, st_b
 
 def pb(st_a, st_b):
     if base_funct.f_len(st_a) != 0:
@@ -46,7 +34,4 @@ def pb(st_a, st_b):
             c.append(st_b[i])
         st_b = c
         st_a = base_funct.f_del_first(st_a)
-        print("pb")
-        return st_a, st_b
-    else:
-        return st_a, st_b
+    return st_a, st_b
